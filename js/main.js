ec8c436655a489ebe9459eaf4265ab6a9d566f04
Vlasta Vetrae <vlastavetra@gmail.com>
2020-10-21 22:06:50 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/main.js
+++ js/main.js
@@ -13,6 +13,33 @@
   const FRAGMENT = document.createDocumentFragment();
   const OFFERS_NUMBER = 8;
 
+  const successHandler = (offers) => {
+    if (offers.length >= OFFERS_NUMBER) {
+      for (let i = 0; i < OFFERS_NUMBER; i++) {
+        FRAGMENT.appendChild(window.pin.renderPin(offers[i]));
+      }
+    } else {
+      for (let i = 0; i < offers.length; i++) {
+        FRAGMENT.appendChild(window.pin.renderPin(offers[i]));
+      }
+    }
+
+    MAP_PINS.appendChild(FRAGMENT);
+  };
+
+  const errorHandler = (errorMessage) => {
+    const node = document.createElement(`div`);
+
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+    node.textContent = errorMessage;
+
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
   let activatePage = (evt) => {
     if (evt.button === 0 || evt.key === `Enter`) {
       window.util.showElement(MAP, MAP_TUMBLER);
@@ -22,17 +49,11 @@
     }
   };
 
-  let offersList = window.mock.renderOffersList(OFFERS_NUMBER);
-
-  for (let i = 0; i < offersList.length; i++) {
-    FRAGMENT.appendChild(window.pin.renderPin(offersList[i]));
-  }
-
-  MAP_PINS.appendChild(FRAGMENT);
-
   window.util.disableElementsTumbler(MAP_FILTERS_CHILDS, true);
   window.util.disableElementsTumbler(AD_FORM_FILDSETS, true);
 
   MAP_PIN_MAIN.addEventListener(`mousedown`, activatePage);
   MAP_PIN_MAIN.addEventListener(`keydown`, activatePage);
+
+  window.backend.load(successHandler, errorHandler);
 })();
