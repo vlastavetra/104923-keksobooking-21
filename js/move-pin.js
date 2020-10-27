936ea246ba4aee5f2f14c4c92277d132d1a9bdd1
Vlasta Vetrae <vlastavetra@gmail.com>
2020-10-27 23:56:35 +0300

    Правки по Личный проект: больше деталей (часть 2)

--- js/move-pin.js
+++ js/move-pin.js
@@ -2,8 +2,16 @@
 
 (() => {
   const PinSize = {
-    width: 50,
+    width: 64,
     height: 70
+  };
+  const YLimit = {
+    MIN: 130,
+    MAX: 630
+  };
+  const XLimit = {
+    MIN: 0 - PinSize.width / 2,
+    MAX: window.const.MAP.clientWidth
   };
 
   let setAddress = (element, x, y) => {
@@ -34,8 +42,13 @@
         y: moveEvt.clientY
       };
 
-      window.const.MAP_PIN_MAIN.style.top = (window.const.MAP_PIN_MAIN.offsetTop - shift.y) + `px`;
-      window.const.MAP_PIN_MAIN.style.left = (window.const.MAP_PIN_MAIN.offsetLeft - shift.x) + `px`;
+      if ((window.const.MAP_PIN_MAIN.offsetLeft - shift.x) >= XLimit.MIN && (window.const.MAP_PIN_MAIN.offsetLeft - shift.x + (PinSize.width / 2)) <= XLimit.MAX) {
+        window.const.MAP_PIN_MAIN.style.left = (window.const.MAP_PIN_MAIN.offsetLeft - shift.x) + `px`;
+      }
+
+      if ((window.const.MAP_PIN_MAIN.offsetTop - shift.y + PinSize.height) >= YLimit.MIN && (window.const.MAP_PIN_MAIN.offsetTop - shift.y + PinSize.height) <= YLimit.MAX) {
+        window.const.MAP_PIN_MAIN.style.top = (window.const.MAP_PIN_MAIN.offsetTop - shift.y) + `px`;
+      }
 
       pinAddressX = parseInt(window.const.MAP_PIN_MAIN.style.left, 10) + (PinSize.width / 2);
       pinAddressY = parseInt(window.const.MAP_PIN_MAIN.style.top, 10) + PinSize.height;
